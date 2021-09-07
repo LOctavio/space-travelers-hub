1caaa64852cd7ba4ae524a6993d397555287fcb4
LOctavio <luis_ram1997@hotmail.com>
2021-09-07 16:48:40 -0500

    Add route and view for myProfile component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Header from './components/Header/Header';
+import Profile from './components/Profile';
 
 function App() {
   return (
     <div className="App">
-      <Header />
+      <Router>
+        <Header />
+        <Switch>
+          <Route path="/my-profile">
+            <Profile />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
