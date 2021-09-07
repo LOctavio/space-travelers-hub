ef7f8ea18aa100d330c8ee4aa7674ccfe4726f5a
Francis Uloko <francisuloko@yahoo.com>
2021-09-07 20:28:05 +0100

    Fix linters errors

---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from '../assets/images/space-travellers-hub-logo.png';
+
+function Header() {
+  return (
+    <header>
+      <img src={logo} alt="space travellers hub logo" />
+      <nav />
+    </header>
+  );
+}
+
+export default Header;
