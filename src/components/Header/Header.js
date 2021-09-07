9c4f5a01b3d4a636c0b6ebb1cc0fa9265f8dbbfe
Francis Uloko <francisuloko@yahoo.com>
2021-09-07 20:55:58 +0100

    Add styling to Header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import logo from '../assets/images/space-travellers-hub-logo.png';
+import logo from '../../assets/images/space-travellers-hub-logo.png';
 
 function Header() {
   return (
-    <header>
-      <img src={logo} alt="space travellers hub logo" />
-      <nav />
+    <header className="space-header">
+      <div className="site-title">
+        <img className="logo" src={logo} alt="space travellers hub logo" />
+        <span className="title">Space Traveller&apos;s Hub</span>
+      </div>
+      <nav className="space-nav" />
     </header>
   );
 }
