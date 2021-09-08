e4185ac5ac5809d08ed3c69622b3c95d246399c8
Luis Octavio Ramirez Cruz <luis_ram1997@hotmail.com>
2021-09-08 16:18:30 -0500

    Revert "Display missions"

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,30 +1,3 @@
-import { useSelector } from 'react-redux';
-import { Table } from 'react-bootstrap';
-import Mission from './Mission';
-
-const Missions = () => {
-  const mission = useSelector((state) => state.missionsReducer);
-  return (
-    <Table className="missions-list" bordered hover>
-      <thead>
-        <tr>
-          <th>Mission</th>
-          <th>Description</th>
-          <th>Status</th>
-          <th> </th>
-        </tr>
-      </thead>
-      <tbody>
-        {mission.map((mission) => (
-          <Mission
-            key={mission.mission_id}
-            name={mission.mission_name}
-            description={mission.description}
-          />
-        ))}
-      </tbody>
-    </Table>
-  );
-};
+const Missions = () => (<h1>Missions</h1>);
 
 export default Missions;
