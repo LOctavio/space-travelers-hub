c734a16348fc8beda8eafa4ddb4bc174aaccad75
Francis Uloko <francisuloko@yahoo.com>
2021-09-09 00:42:14 +0100

    Add Button and Badge components

--- src/components/Rockets.js
+++ src/components/Rockets.js
@@ -4,23 +4,22 @@ import Rocket from './Rocket';
 import rocketHooks from '../hooks/rockets';
 
 const Rockets = () => {
-  const rockets = useSelector((state) => state.rockets);
+  const rockets = useSelector((state) => state.rocketsReducer);
 
   rocketHooks();
 
   return (
     <div>
-      {
-        rockets.map((rocket) => 
-          <Rocket
-            key={rocket.id}
-            id={rocket.id}
-            name={rocket.name}
-            description={rocket.description}
-            image={rocket.imageUrl}
-          />
-        )
-      }
+      {rockets.map((rocket) => (
+        <Rocket
+          key={rocket.id}
+          id={rocket.id}
+          name={rocket.name}
+          description={rocket.description}
+          image={rocket.image}
+          reserved={rocket.reserved}
+        />
+      ))}
     </div>
   );
 };
