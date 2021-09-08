cf8d062e907fa95aa2d91203398f5951bd749cdf
Francis Uloko <francisuloko@yahoo.com>
2021-09-08 23:15:24 +0100

    Modify Rockets component

--- src/components/Rockets.js
+++ src/components/Rockets.js
@@ -1,10 +1,27 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+import Rocket from './Rocket';
 import rocketHooks from '../hooks/rockets';
 
 const Rockets = () => {
+  const rockets = useSelector((state) => state.rockets);
+
   rocketHooks();
 
   return (
-    <h1>Rockets</h1>
+    <div>
+      {
+        rockets.map((rocket) => 
+          <Rocket
+            key={rocket.id}
+            id={rocket.id}
+            name={rocket.name}
+            description={rocket.description}
+            image={rocket.imageUrl}
+          />
+        )
+      }
+    </div>
   );
 };
 
