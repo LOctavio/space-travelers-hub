7e1a158c8b6511a47a98eae7b25d2abd509d15e2
LOctavio <luis_ram1997@hotmail.com>
2021-09-08 13:51:54 -0500

    Fetch data from the Missions endpoint

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Header from './components/Header/Header';
 import Profile from './components/Profile';
 import Missions from './components/Missions';
 
 function App() {
+  const [missions, setMissions] = useState([]);
+
+  const getMissionsList = async () => {
+    await fetch('https://api.spacexdata.com/v3/missions')
+      .then((response) => response.json())
+      .then((json) => {
+        setMissions(json);
+      });
+  };
+
+  useEffect(() => {
+    getMissionsList();
+  }, []);
+
   return (
     <div className="App">
       <Router>
