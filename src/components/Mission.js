bee29cbc3b9452510bcae173c02b3e0f593c2f53
LOctavio <luis_ram1997@hotmail.com>
2021-09-09 11:19:41 -0500

    Implement mision joining

--- src/components/Mission.js
+++ src/components/Mission.js
@@ -1,8 +1,12 @@
 import PropTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
 import { Button } from 'react-bootstrap';
+import { joinMission } from '../redux/missions/missions';
 
 const Mission = (props) => {
-  const { description, name } = props;
+  const dispatch = useDispatch();
+
+  const { description, name, id } = props;
   return (
     <tr>
       <th>{name}</th>
@@ -11,7 +15,7 @@ const Mission = (props) => {
         <Button className="member-button" variant="secondary">NOT A MEMBER</Button>
       </td>
       <td className="button-container">
-        <Button className="join-mission-button" variant="outline-secondary">JOIN MISSION</Button>
+        <Button className="join-mission-button" variant="outline-secondary" onClick={() => dispatch(joinMission(id))}>JOIN MISSION</Button>
       </td>
     </tr>
   );
@@ -20,6 +24,7 @@ const Mission = (props) => {
 Mission.propTypes = {
   description: PropTypes.string.isRequired,
   name: PropTypes.string.isRequired,
+  id: PropTypes.string.isRequired,
 };
 
 export default Mission;
